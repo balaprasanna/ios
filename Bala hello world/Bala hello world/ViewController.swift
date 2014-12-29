b0245b475ea4735abf9b709b18745ddbe723ee49
balaprasanna <balaprasannav2009@gmail.com>
2014-12-29 23:49:14 +0800

    test commit on 29/12/2014

--- Bala hello world/Bala hello world/ViewController.swift
+++ Bala hello world/Bala hello world/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingInProgress: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func btnRecord(sender: UIButton) {
+     // To DO : set the text to "Recording in progress"
+        recordingInProgress.hidden = false
+     //       : start record the audio
+        println("in audio event handler")
+    }
 
+    @IBAction func btnStopRecorder(sender: UIButton) {
+        // TO DO : Hide the text (recordingInProgress) - ""
+        recordingInProgress.hidden = true
+    }
 }
 
---
+++ Bala playground.playground/section-1.swift
@@ -0,0 +1,22 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+var android = "weak"
+println(android)
+var myint : Int = 1
+var mydouble : Double = 1.5
+var mybool : Bool = true
+var myname : String = "Prasanna"
+
+var myInt = 1
+var myName = "Prasanna"
+var myDouble = 1.5
+var myBool = true
+
+let myconstant = 10
+
+println(myconstant)
+
+var nillvariable : Int ? nillvariable = nil
